e13856ee1573a04eb9cc5cd8958684a063e3ade4
13166253532 <13166253532@163.com>
2017-12-12 14:53:43 +0800

    add init

---
+++ SHYApproved/SHYApproved/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SHYApproved
+//
+//  Created by xiejiangbo on 2017/12/12.
+//  Copyright © 2017年 yin chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
